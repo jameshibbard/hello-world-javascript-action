bceba572bdecfc6938df2563e7051ba0cfbd3ada
James Hibbard <1940994+jameshibbard@users.noreply.github.com>
2020-06-15 14:58:55 +0200

    Correct syntax error

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
+const axios = require('axios');
 const core = require('@actions/core');
 const github = require('@actions/github');
-const axios = require('axios');
 
-try {
-  // Get list of files that have changed
-  console.log("Fetching commit...")
-  const url = `https://api.github.com/repos/jameshibbard/test-hello-world-javascript-action/commits/${process.env.GITHUB_SHA}`;
-  console.log(url);
-  const res = await axios.get(url);
-  res.data.files.forEach((file) => {
-    console.log(file.filename);
-  });
+async function run() {
+  try {
+    // Get list of files that have changed
+    console.log('Fetching commit...');
+    const url = `https://api.github.com/repos/jameshibbard/test-hello-world-javascript-action/commits/${process.env.GITHUB_SHA}`;
+    console.log(url);
+    const res = await axios.get(url);
+    res.data.files.forEach((file) => {
+      console.log(file.filename);
+    });
 
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
 
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
